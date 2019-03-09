7313a488d72ab94facdd85a07f244d0a61e07798
Manuel Osorio <manuelosorio.mkdesignz@gmail.com>
2019-03-09 11:52:41 -0500

    Optimize images, validat html and css, fix broken images

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var browserSync = require('browser-sync').create();
+var imagemin = require('gulp-imagemin');
 
 // Task for building blog when something changed:
 gulp.task('build', shell.task(['bundle exec jekyll build --watch']));
@@ -17,5 +18,15 @@ gulp.task('serve', function () {
     // Reloads page when some of the already built files changed:
     gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
-
+// Compress Images
+gulp.task('images', function() {
+  gulp.src('assets/img/**/**/*')
+    .pipe(imagemin({
+      interlaced: true,
+      progressive: true,
+      optimizationLevel: 8
+  }))
+    .pipe(gulp.dest('_site/assets/img/'))
+    .pipe(browserSync.stream());
+});
 gulp.task('default', ['build', 'serve']);
